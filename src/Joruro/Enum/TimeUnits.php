16ac860b572430d5f8d9aa22cdfc3c523309c299
joruro <joruro@joruro.me>
2016-06-11 16:12:47 +0000

    Added Enum class support

---
+++ src/Joruro/Enum/TimeUnits.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Joruro\Enum;
+
+/**
+ * Class TimeUnits
+ * @package Joruro\Enum
+ */
+class TimeUnits extends Enum
+{
+    const SECONDS = 's';
+    const MILLISECONDS = 'ms';
+}
